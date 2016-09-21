a83e59da2e1cc053c765ced506f0009107d950ff
Andy <andrew.sunglae.kim@gmail.com>
2016-09-20 23:25:50 -0400

    sending correct props to react component

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,7 +2,10 @@ class GameController < ApplicationController
   before_action :authenticate_user!
   def homebase
     @user = current_user
-    @hello_world_props = { name: "Stranger" }
     @player_items = current_user.player_items
+    @homebase_props = {
+      user: @user,
+      player_items: @player_items
+    }
   end
 end
--- client/app/bundles/HomeBase/containers/HomeBase.jsx
+++ client/app/bundles/HomeBase/containers/HomeBase.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 // Simple example of a React "smart" component
 class HomeBase extends Component {
   constructor(props, context) {
+    console.log("hello");
+    console.log(props);
     super(props, context);
     this.state = {};
   }

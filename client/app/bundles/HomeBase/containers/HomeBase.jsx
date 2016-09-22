72b4dbbe188db6780209ed59670824b750e1c5ad
Andy <andrew.sunglae.kim@gmail.com>
2016-09-21 22:00:47 -0400

    adds user inventory container

--- client/app/bundles/HomeBase/containers/HomeBase.jsx
+++ client/app/bundles/HomeBase/containers/HomeBase.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
+import UserInventory from '../../Inventory/containers/UserInventory'
+
 // Simple example of a React "smart" component
 class HomeBase extends Component {
   constructor(props, context) {
     console.log("hello");
     console.log(props);
+    console.log(context);
     super(props, context);
     this.state = {};
   }
@@ -11,6 +14,7 @@ class HomeBase extends Component {
     return (
       <div>
         This is the homebase
+        <UserInventory />
       </div>
     );
   }
---
+++ client/app/bundles/Inventory/containers/UserInventory.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class UserInventory extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>
+        this is the inventory
+      </div>
+    )
+  }
+}
+
+export default UserInventory

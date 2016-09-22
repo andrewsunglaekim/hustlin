a652a18fe8b5da848edd07c203ff0c79ca8cb2b0
Andy <andrew.sunglae.kim@gmail.com>
2016-09-21 23:47:41 -0400

    refactors getting player_items in backend

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,14 +2,7 @@ class GameController < ApplicationController
   before_action :authenticate_user!
   def homebase
     @user = current_user
-    @player_items = current_user.player_items.map do |player_item|
-      {
-        id: player_item.id,
-        name: player_item.item.name,
-        quantity: player_item.quantity,
-        value: player_item.item.value
-      }
-    end
+    @player_items = get_player_items
     @homebase_props = {
       user: @user,
       player_items: @player_items
@@ -17,14 +10,7 @@ class GameController < ApplicationController
   end
 
   def store
-    @player_items = current_user.player_items.map do |player_item|
-      {
-        id: player_item.id,
-        name: player_item.item.name,
-        quantity: player_item.quantity,
-        value: player_item.item.value
-      }
-    end
+    @player_items = get_player_items
     # TODO: obviously can't do Store.first, make multiple stores going forward, open up as you progress in the game
     @store = Store.first
     @store_items = @store.store_items.map do |store_item|
@@ -41,5 +27,16 @@ class GameController < ApplicationController
       player_items: @player_items,
       store_items: @store_items
     }
+  end
+  private
+  def get_player_items
+    current_user.player_items.map do |player_item|
+      {
+        id: player_item.id,
+        name: player_item.item.name,
+        quantity: player_item.quantity,
+        value: player_item.item.value
+      }
+    end
   end
 end

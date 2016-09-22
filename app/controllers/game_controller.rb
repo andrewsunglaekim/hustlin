cc41ebe5d8a056fbbc3b7b181ce40b51dc416fc1
Andy <andrew.sunglae.kim@gmail.com>
2016-09-21 22:53:44 -0400

    Removes turbo links to remove unmounting error

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -17,6 +17,19 @@ class GameController < ApplicationController
   end
 
   def store
+    @user = current_user
+    @player_items = current_user.player_items.map do |player_item|
+      {
+        id: player_item.id,
+        name: player_item.item.name,
+        quantity: player_item.quantity,
+        value: player_item.item.value
+      }
+    end
+    # TODO: obviously can't do Store.first, make multiple stores going forward, open up as you progress in the game
+    @store = Store.first
+    @store_items = @store.store_items.map do |store|
 
+    end
   end
 end

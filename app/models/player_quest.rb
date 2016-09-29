7f620d0d3a799e6bb532c48a00f34d4be489132f
Andy <andrew.sunglae.kim@gmail.com>
2016-09-28 21:32:51 -0400

    player quests now update its starting age based on player age

--- app/models/player_quest.rb
+++ app/models/player_quest.rb
@@ -1,9 +1,9 @@
 class PlayerQuest < ApplicationRecord
-  belongs_to :player
+  belongs_to :user
   belongs_to :quest
   before_save :update_starting_age
 
   def update_starting_age
-
+    self.starting_age = self.user.age
   end
 end
